0767cac2a3a8f5d485501da6ee6555672188ebb3
drewholowaty <drewholowaty@gmail.com>
2025-11-24 15:44:26 -0500

    added scripts used for custom keyboard shortcuts

---
+++ .local/bin/create_new_writing_entry.sh
@@ -0,0 +1,1 @@
+ptyxis --working-directory googledrive/documents/art/review-queue -e nvim $(date +\%Y-\%m-\%d).txt 
---
+++ .local/bin/open_notes.sh
@@ -0,0 +1,1 @@
+ptyxis --working-directory googledrive/documents/notes -e nvim notes.md
